f185f9c38067e6ce56f9d0ce5ae9b29bbb9b8e63
Henrik256 <149734670+Henrik256@users.noreply.github.com>
2024-04-24 21:06:48 -0500

    Added clicker upgrades and the very basic framework for a menu

--- PiggyClicker.py
+++ PiggyClicker.py
@@ -1,11 +1,16 @@
 import json
 import time
 import threading
-filename = "JsonFiles\PiggyPrices.json"
+import sys
+buildingFilename = "JsonFiles\PiggyPrices.json"
+clickerFilename = "JsonFiles\ClickerUpgrades.json"
 balance = 0
 moneyPerClick = 1
 moneyPerSecond = 0
-def getFile():
+
+sys.setrecursionlimit(999999999)
+
+def getFile(filename):
     try:
         with open(filename, 'r') as f:
             data = json.load(f)
@@ -15,7 +20,17 @@ def getFile():
     except json.JSONDecodeError:
         print(f"Error: Invalid JSON format in file {filename}.")
 
-buildingData = getFile()
+buildingData = getFile(buildingFilename)
+clickerData = getFile(clickerFilename)
+
+def menu():
+    global balance
+    global moneyPerClick
+    global moneyPerSecond
+    hasAccount = input("Do you have an account already? ")
+    if hasAccount == "Yes" or hasAccount == "yes" or hasAccount == "y":
+        username = input("What is the username? ")
+        password = input("What is the password? ")
 
 def perSecond():
     global balance
@@ -35,6 +50,8 @@ def decide():
     decision = input("What would you like to do? ").strip()
     if decision == "1":
         click()
+    elif decision == "2":
+        purchaseClickerUpgrades()
     elif decision == "3":
         purchaseBuildings()
     elif decision == "4":
@@ -50,6 +67,32 @@ def click():
     balance += moneyPerClick
     decide()
 
+def purchaseClickerUpgrades():
+    global moneyPerClick
+    print(f"${clickerData[0]["Price"]} {clickerData[0]["Name"]} ${clickerData[0]["MPC"]}/sec")
+    print(f"${clickerData[1]["Price"]} {clickerData[1]["Name"]} ${clickerData[1]["MPC"]}/sec")
+    print(f"${clickerData[2]["Price"]} {clickerData[2]["Name"]} ${clickerData[2]["MPC"]}/sec")
+    print(f"${clickerData[3]["Price"]} {clickerData[3]["Name"]} ${clickerData[3]["MPC"]}/sec")
+    print(f"${clickerData[4]["Price"]} {clickerData[4]["Name"]} ${clickerData[4]["MPC"]}/sec")
+    print(f"${clickerData[5]["Price"]} {clickerData[5]["Name"]} ${clickerData[5]["MPC"]}/sec")
+    try:
+        purchaseDecision = int(input("What would you like to buy? ").strip())
+        clickerData[purchaseDecision - 1]["Bought"] = True
+        moneyPerClick += clickerData[purchaseDecision - 1]["MPC"]
+        with open(clickerFilename, 'w') as clickerDataFile:
+            clickerDataFile.write(str(clickerData).replace("'", '"').replace("True", "true").replace("False", "false"))
+            clickerDataFile.close()
+            print("Purchase complete!")
+            time.sleep(1.5)
+            decide()
+    except ValueError:
+        print("Please enter a valid number")
+        time.sleep(4)
+        purchaseClickerUpgrades()
+    except IndexError:
+        print("Please enter a valid number")
+        time.sleep(4)
+        purchaseClickerUpgrades()
 
 def purchaseBuildings():
         print(f"${buildingData[0]["Price"]:.2f} {buildingData[0]["Name"]}: ${buildingData[0]["MPS"]:.2f}/sec (You have {buildingData[0]["Amount"]})")
@@ -58,7 +101,7 @@ def purchaseBuildings():
         try:
             purchaseDecision = int(input("What would you like to buy? ").strip())
             buildingData[purchaseDecision - 1]["Amount"] = buildingData[purchaseDecision - 1]["Amount"] + 1
-            with open(filename, 'w') as buildingDataFile:
+            with open(buildingFilename, 'w') as buildingDataFile:
                 buildingDataFile.write(str(buildingData).replace("'", '"').replace("True", "true").replace("False", "false"))
                 buildingDataFile.close()
                 decide()
@@ -85,7 +128,7 @@ def purchaseBuildingUpgrades():
                 selectedBuildingUpgrade = int(input("Which upgrade do you want? "))
                 buildingData[selectedBuilding - 1]["Upgrades"][selectedBuildingUpgrade - 1]["Bought"] = True
                 buildingData[selectedBuilding - 1]["MPS"] *= buildingData[selectedBuilding - 1]["Upgrades"][selectedBuildingUpgrade - 1]["Multiplier"]
-                with open(filename, 'w') as buildingDataFile:
+                with open(buildingFilename, 'w') as buildingDataFile:
                     buildingDataFile.write(str(buildingData).replace("'", '"').replace("True", "true").replace("False", "false"))
                     buildingDataFile.close()
                     decide()
@@ -113,6 +156,8 @@ def checkStats():
         print(f"You make ${moneyPerSecond:.2f} every second!")
         time.sleep(6)
         decide()
+
+menu()
 
 perSecondThread = threading.Thread(target = perSecond)
 decideThread = threading.Thread(target = decide)
